e3916f5784bd49c5878213b3fe4b932987b34745
Neilson Gamble <neilsongamble@scandy-mac-pro.local>
2019-07-01 16:51:45 -0500

    stopping for now, may come back to

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+// import { Button, Container, Content, Text, StyleProvider } from "native-base";
 import {
   Container,
   Header,
@@ -9,12 +10,17 @@ import {
   Button,
   Left,
   Right,
+  H1,
   Body,
+  StyleProvider,
   Icon,
   Text
 } from "native-base";
+import { PixelRatio } from "react-native";
+import getTheme from "./native-base-theme/components";
+import sdc from "./native-base-theme/variables/sdc";
+
 import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
 
 export default class AnatomyExample extends Component {
   state = {
@@ -22,9 +28,8 @@ export default class AnatomyExample extends Component {
   };
   async componentDidMount() {
     await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      ...Ionicons.font
+      SofiaProRegular: require("./assets/fonts/SophiaPro/SofiaProRegular.otf"),
+      SofiaProBlack: require("./assets/fonts/SophiaPro/SofiaProBlack.otf")
     });
     this.setState({ isReady: true });
   }
@@ -32,31 +37,28 @@ export default class AnatomyExample extends Component {
   render() {
     const { isReady } = this.state;
 
+    let foo = PixelRatio.get();
+    console.log(foo);
+
     return (
       isReady && (
-        <Container>
-          <Header>
-            <Left>
-              <Button transparent>
-                <Icon name="menu" />
-              </Button>
-            </Left>
-            <Body>
-              <Title>Header</Title>
-            </Body>
-            <Right />
-          </Header>
-          <Content>
-            <Text>This is Content Section</Text>
-          </Content>
-          <Footer>
-            <FooterTab>
-              <Button full>
-                <Text>Footer</Text>
-              </Button>
-            </FooterTab>
-          </Footer>
-        </Container>
+        <StyleProvider style={getTheme(sdc)}>
+          <Container>
+            <Header transparent>
+              <Title>SmileDirectClub</Title>
+            </Header>
+            <Content>
+              <Text>This is Content Section</Text>
+            </Content>
+            <Footer>
+              <FooterTab>
+                <Button full>
+                  <Text>Footer</Text>
+                </Button>
+              </FooterTab>
+            </Footer>
+          </Container>
+        </StyleProvider>
       )
     );
   }
